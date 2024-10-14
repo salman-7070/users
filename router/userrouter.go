44c4ed1444caa51d456a71f73d79040f379cadb5
salman-7070 <salmanasharaf007@gmail.com>
2024-10-14 15:49:31 +0400

    had some changes in router

--- router/userrouter.go
+++ router/userrouter.go
@@ -10,10 +10,10 @@ import (
 func SetUserRouter(router *mux.Router) *mux.Router {
 	myControllers := &controller.UserController{}
 
-	router.Handle("/students/create", http.HandlerFunc(myControllers.Create)).Methods("POST")
-	router.Handle("/students/update", http.HandlerFunc(myControllers.Update)).Methods("POST")
-	router.Handle("/students/getbyname", http.HandlerFunc(myControllers.GetAllUserData)).Methods("POST")
-	router.Handle("/students/getbyaddress", http.HandlerFunc(myControllers.Delete)).Methods("POST")
+	router.Handle("/user/create", http.HandlerFunc(myControllers.Create)).Methods("POST")
+	router.Handle("/user/update", http.HandlerFunc(myControllers.Update)).Methods("POST")
+	router.Handle("/user/getbyname", http.HandlerFunc(myControllers.GetAllUserData)).Methods("POST")
+	router.Handle("/user/getbyaddress", http.HandlerFunc(myControllers.Delete)).Methods("POST")
 
 	return router
 }
